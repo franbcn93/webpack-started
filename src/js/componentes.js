ffdede60190df82c29caf6a64dd60c2df7784394
Francisco <franciscoparisalbero@gmail.com>
2021-02-16 11:26:01 +0100

    Iniciliacion del projecto

---
+++ src/js/componentes.js
@@ -0,0 +1,8 @@
+import "../css/componentes.css";
+
+export const saludar = (nombre) => {
+  console.log("Creando etiqueta h1");
+  const h1 = document.createElement("h1");
+  h1.innerText = `Hello, ${nombre}, como estás?`;
+  document.body.append(h1);
+};
